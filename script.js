1a0ab4b31f7151b330eb57fd030f62ed573adb41
Mohid Yaseen <mmohid069@gmail.com>
2026-07-23 17:27:25 +0500

    feat: implement operator selection and state update

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const display = document.getElementById("display");
 
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
 
 let state = {
   currentValue: "",
   previousValue: null,
   operator: null,
+};
+
+const operatorMap = {
+  "+": "+",
+  "-": "-",
+  "÷": "/",
+  "×": "*",
 };
 
 function updateDisplay() {
@@ -15,6 +23,18 @@ function updateDisplay() {
 numberButtons.forEach(function (button) {
   button.addEventListener("click", function () {
     state = { ...state, currentValue: state.currentValue + this.textContent };
+    updateDisplay();
+  });
+});
+
+operatorButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    state = {
+      ...state,
+      previousValue: state.currentValue,
+      operator: operatorMap[this.textContent],
+      currentValue: "",
+    };
     updateDisplay();
   });
 });
