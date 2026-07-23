de1aea121c660849a09d296afefeedfc91dc8e3a
Mohid Yaseen <mmohid069@gmail.com>
2026-07-23 15:40:39 +0500

    feat: append clicked digit to display

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const display = document.getElementById("display");
 
 const numberButtons = document.querySelectorAll(".number");
+
+numberButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    display.textContent = display.textContent + this.textContent;
+  });
+});
